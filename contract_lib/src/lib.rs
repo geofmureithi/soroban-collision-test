d2d6bd4679d4f22486c0f2b79d179e098ebfe1af
Geoffrey Mureithi <mureithinjuguna@gmail.com>
2023-09-14 16:29:36 +0300

    chore: proof keys dont conflict

--- contract_lib/src/lib.rs
+++ contract_lib/src/lib.rs
@@ -1,8 +1,13 @@
 #![no_std]
 
-use soroban_sdk::{symbol_short, Env, Symbol, contract, contractimpl};
+use soroban_sdk::{contract, contractimpl, symbol_short, Env, Symbol};
 
 const COUNTER: Symbol = symbol_short!("COUNTER");
+
+#[soroban_sdk::contracttype]
+pub enum DataKey {
+    InnerCounter,
+}
 
 #[contract]
 pub struct Counter;
@@ -11,12 +16,18 @@ pub struct Counter;
 impl Counter {
     pub fn init(env: Env) {
         env.storage().persistent().set(&COUNTER, &0u32);
+        env.storage()
+            .persistent()
+            .set(&DataKey::InnerCounter, &0u32)
     }
 
     pub fn increment(env: Env, by: u32) -> u32 {
         let mut count = env.storage().persistent().get::<_, u32>(&COUNTER).unwrap();
         count += by;
         env.storage().persistent().set(&COUNTER, &count);
+        env.storage()
+            .persistent()
+            .set(&DataKey::InnerCounter, &count);
         count
     }
 
